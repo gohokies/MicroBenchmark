f2f5554c74b2f712b96c62cd5d859b8671ac84ef
gohokies <cpapideveloper@gmail.com>
2024-09-27 23:43:49 -0700

    Add skeleton of project

---
+++ scripts/vcpkg.ps1
@@ -0,0 +1,43 @@
+param(
+    [String]$TargetTriplet = ""
+)
+. $PSScriptRoot/utilities.ps1
+
+$RootDir = Resolve-Path $PSScriptRoot/..
+
+Push-Location $RootDir/vcpkg
+
+# $IsLinux or $IsMacOS is available when $PSVersionTable.PSVersion.Major -ge 6
+if ($IsLinux){
+    # On a Linux host
+    if (-Not (Test-Path -Path "$RootDir/vcpkg/vcpkg")){
+        & ./bootstrap-vcpkg.sh
+        & ./vcpkg integrate install
+    }
+}
+elseif ($IsMacOS){
+    # On a MacOS host
+    if (-Not (Test-Path -Path "$RootDir/vcpkg/vcpkg")){
+        & ./bootstrap-vcpkg.sh --allowAppleClang
+        & ./vcpkg integrate install
+    }
+}
+else{
+    # On a Windows host
+    if (-Not (Test-Path -Path "$RootDir/vcpkg/vcpkg.exe")){
+        & .\bootstrap-vcpkg.bat
+        & .\vcpkg.exe integrate install
+    }
+}
+
+if ($TargetTriplet -eq ""){
+    $TargetPlatform = GetHostOS
+    $TargetArch = GetHostArch
+    $TargetTriplet = "$TargetArch-$TargetPlatform"
+}
+
+Write-Host "Installing vcpkg components for $TargetTriplet"
+
+& ./vcpkg install --Triplet $TargetTriplet | Write-Host
+
+Pop-Location
